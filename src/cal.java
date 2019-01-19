1b495a95d0c8f0b5149336c62b0589d77611465c
Mohammad <mwmirza05@gmail.com>
2019-01-19 15:58:05 -0600

    committing changes of MM

--- src/cal.java
+++ src/cal.java
@@ -1,5 +1,7 @@
 public class cal {
 
     // check 1
-    // change 2 just now - delete this Mr. - what the heck you are doing?? 
+    // change 2 just now - delete this Mr. - what the heck you are doing??
+
+    // change by MM
 }
